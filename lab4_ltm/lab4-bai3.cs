84984a3774ebe54973cc997a5cc795e471766731
20520796 <104158070+20520796@users.noreply.github.com>
2022-05-17 22:35:13 +0700

    Update lab4-bai3.cs

--- lab4_ltm/lab4-bai3.cs
+++ lab4_ltm/lab4-bai3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace lab4_ltm
 {
-    public partial class lab4_bai3 : Form
+    public partial class BAI03 : Form
     {
-        public lab4_bai3()
+        public Bai03()
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rtbContent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "HTML|*.html";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                tbPath.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void btDOWLOAD_Click(object sender, EventArgs e)
+        {
+            if (tbPath.Text == "" || tbURL.Text == " ")
+                MessageBox.Show("Ô Path hoặc URL đang để trống!", "THÔNG BÁO", MessageBoxButtons.OK);
+
+            WebClient client = new WebClient();
+            client.DownloadFile(tbURL.Text, tbPath.Text);
+            rtbContent.Text = client.DownloadString(tbURL.Text);
+        }
+
     }
 }
